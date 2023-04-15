5f12f7668ee99ed4a7f080b73e1f3ae1d9ad1e39
laba <labanoelie@gmail.com>
2023-04-15 12:05:43 -0400

    new testing

--- variables.tf
+++ variables.tf
@@ -6,21 +6,25 @@ variable "aws_region" {
 variable "environment" {
   description = "The environment name to use in resource names."
   type        = string
+  default     = ""
 }
 
 variable "vpc_cidr_block" {
   description = "The CIDR block to use for the VPC."
   type        = string
+  default     = ""
 }
 
 
 variable "public_subnet_cidr_blocks" {
   description = "Available cidr blocks for public subnets."
   type        = string
+  default     = ""
 }
 
 variable "private_subnet_cidr_blocks" {
   description = "Available cidr blocks for private subnets."
   type        = string
+  default     = ""
 }
 
